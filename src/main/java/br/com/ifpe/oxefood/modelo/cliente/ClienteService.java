e18215ac6a086f8ff398e98b92e06bf44f82f415
Roberto Alencar <rroberto.alencar@gmail.com>
2026-03-04 20:29:35 -0300

    enviando arquivos do projeto

---
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
@@ -0,0 +1,46 @@
+package br.com.ifpe.oxefood.modelo.cliente;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repository;
+
+    @Transactional
+    public Cliente save(Cliente cliente) {
+
+        cliente.setHabilitado(Boolean.TRUE);
+        return repository.save(cliente);
+    }
+
+    @Transactional
+   public void update(Long id, Cliente clienteAlterado) {
+
+        Cliente cliente = repository.findById(id).get();
+        cliente.setNome(clienteAlterado.getNome());
+        cliente.setDataNascimento(clienteAlterado.getDataNascimento());
+        cliente.setCpf(clienteAlterado.getCpf());
+        cliente.setFoneCelular(clienteAlterado.getFoneCelular());
+        cliente.setFoneFixo(clienteAlterado.getFoneFixo());
+            
+        repository.save(cliente);
+    }
+
+    public List<Cliente> listarTodos() {
+  
+        return repository.findAll(); //SELECT * FROM Cliente
+    }
+
+    public Cliente obterPorID(Long id) {
+
+        return repository.findById(id).get(); //SELECT * FROM Cliente WHERE id = ? 
+    }
+
+}
