c61cbe343bea0801e305a4cebcd6c0c071306cf0
Roberto Alencar <rroberto.alencar@gmail.com>
2026-05-06 20:12:29 -0300

    enviando arquivos do projeto

--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.ifpe.oxefood.modelo.acesso.Perfil;
 import br.com.ifpe.oxefood.modelo.acesso.PerfilRepository;
+import br.com.ifpe.oxefood.modelo.acesso.Usuario;
 import br.com.ifpe.oxefood.modelo.acesso.UsuarioService;
 import jakarta.transaction.Transactional;
 
@@ -27,7 +28,7 @@ public class ClienteService {
     private PerfilRepository perfilUsuarioRepository;
 
     @Transactional
-    public Cliente save(Cliente cliente) {
+    public Cliente save(Cliente cliente, Usuario usuarioLogado) {
 
         usuarioService.save(cliente.getUsuario());
 
@@ -37,11 +38,12 @@ public class ClienteService {
         }
 
         cliente.setHabilitado(Boolean.TRUE);
+        cliente.setCriadoPor(usuarioLogado);
         return repository.save(cliente);
     }
 
     @Transactional
-   public void update(Long id, Cliente clienteAlterado) {
+   public void update(Long id, Cliente clienteAlterado, Usuario usuarioLogado) {
 
         Cliente cliente = repository.findById(id).get();
         cliente.setNome(clienteAlterado.getNome());
@@ -49,7 +51,9 @@ public class ClienteService {
         cliente.setCpf(clienteAlterado.getCpf());
         cliente.setFoneCelular(clienteAlterado.getFoneCelular());
         cliente.setFoneFixo(clienteAlterado.getFoneFixo());
-            
+
+        cliente.setUltimaModificacaoPor(usuarioLogado);
+
         repository.save(cliente);
     }
 
